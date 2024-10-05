02cc6acafe4acba8793fe799a3be2286973a0255
Piotr Ranosz <pranosz@wp.pl>
2024-10-05 14:14:45 +0200

    correction

--- cypress/integration/first.js
+++ cypress/integration/first.js
@@ -3,5 +3,7 @@
 describe('E2E - Home Page', () => {
     it('Should open home page - automationpractice', () => {
         cy.visit('/');
+        cy.get('.col-sm-7 > .btn').click();
+        cy.get('.room-booking-form > .form-control').type('Piotr');
     });
 });
